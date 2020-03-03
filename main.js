a7d433fcbb8eb14fe6005b3150c89c3ad1a25caf
ajronn <34678770+ajronn@users.noreply.github.com>
2020-03-03 15:36:58 +0100

    add count of notes in columns

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ function textAreaAdjust(o) {
 	o.style.height = (25+o.scrollHeight)+"px";
 	o.style.width = "100%";
 }
-
+var counts = [0,0,0,0];
 function makenote()
 {
 	document.getElementById("field").innerHTML = '<div class="card p-2 cyan lighten-4 darken-1 mx-2 list-item stickynote" draggable="true"><div class="delnote"></div><h5 class="card-title">Tytuł</h5><textarea class="cyan lighten-4 form-control xd" onkeydown="textAreaAdjust(this)"></textarea></div>';
-
+	
+	
 	$('.delnote').on("click",function(){
+		var tmp = parseInt($(this).parent().parent().attr("id"));
+		counts[tmp]--;
+		alert(counts[tmp]);
 		$(this).parent().remove();
 		return;
 	});
@@ -34,14 +38,46 @@ function makenote()
 
 	});
 
-	$(".list").droppable({
+	$(".backlog").droppable({
 		tolerance: "intersect",
 		accept: ".stickynote",
 		activeClass: "ui-state-default",
 		hoverClass: "ui-state-hover",
 		drop: function(event, ui) {
-			$(this).append($(ui.draggable));
+			$(".backlog").append($(ui.draggable));
+			counts[0]++;
+			alert(counts[0]);
+			
 		}
 	});
-
+	
+	$(".inprogress").droppable({
+		tolerance: "intersect",
+		accept: ".stickynote",
+		activeClass: "ui-state-default",
+		hoverClass: "ui-state-hover",
+		drop: function(event, ui) {
+			$(".inprogress").append($(ui.draggable));
+		}
+	});
+	
+	$(".peerreview").droppable({
+		tolerance: "intersect",
+		accept: ".stickynote",
+		activeClass: "ui-state-default",
+		hoverClass: "ui-state-hover",
+		drop: function(event, ui) {
+			$(".peerreview").append($(ui.draggable));
+		}
+	});
+	
+	$(".intest").droppable({
+		tolerance: "intersect",
+		accept: ".stickynote",
+		activeClass: "ui-state-default",
+		hoverClass: "ui-state-hover",
+		drop: function(event, ui) {
+			$(".intest").append($(ui.draggable));
+		}
+	});
 }
