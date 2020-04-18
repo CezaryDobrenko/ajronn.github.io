3d4dee6549e607c930038980a5ca17fe36f57659
warminski <35105074+warminski@users.noreply.github.com>
2020-04-18 19:53:29 +0200

    added popup window

--- src/components/Swimlane.js
+++ src/components/Swimlane.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Collapsible from 'react-collapsible'
 import Draggable from "../dnd/draggable"
 import Droppable from "../dnd/droppable"
-
-
+import Popup from "reactjs-popup"
 
 class Swimlane extends React.Component{
 
@@ -42,7 +41,10 @@ class Swimlane extends React.Component{
 
        return (
            <div>
-            <Collapsible trigger={<div><input style={{width: "150px", height: "20px"}}type="text" defaultValue={this.props.element.title}></input><button onClick={() => this.props.removeSwimlane(this.props.element)}>Remove swimlane</button><button onClick={() => this.addColumn(this.props.element)}>Add column</button></div>}>
+            <Collapsible trigger={<div><Popup trigger={<button>Trigger</button>} position="right center">
+            <input style={{width: "150px", height: "20px"}}type="text" defaultValue={this.props.element.title}></input>
+            <button onClick={() => this.props.removeSwimlane(this.props.element)}>Remove swimlane</button>
+            <button onClick={() => this.addColumn(this.props.element)}>Add column</button></Popup></div>}>
                 <Draggable id="item1"><div style={{backgroundColor: 'gray', width: '100px'}}>Some text</div></Draggable>
                 <div style={{height: "300px", backgroundColor: "#f3e5ab"}}>{elements}</div>
             </Collapsible>
