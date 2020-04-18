630e3d69eab7ab1c10cd90272a3d48580fe986e4
ajronn <34678770+ajronn@users.noreply.github.com>
2020-04-18 21:58:08 +0200

    add column add field trigger by "enter"

---
+++ src/components/AddColumnInput.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default class AddColumnInput extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    
+    _handleKeyDown = (e) => {
+      if (e.key === 'Enter') {
+        {this.props.addColumn(e.target.value)}
+        e.target.value = ""
+      }
+    }
+  
+    render() {
+      return <input style={{width: "70%", display: "block", marginLeft: "auto", marginRight: "auto"}} type="text" placeholder="Add column" onKeyDown={this._handleKeyDown} />
+    }
+  }
--- src/components/Swimlane.js
+++ src/components/Swimlane.js
@@ -3,6 +3,8 @@ import Collapsible from 'react-collapsible'
 import Draggable from "../dnd/draggable"
 import Droppable from "../dnd/droppable"
 import Popup from "reactjs-popup"
+import AddColumnInput from "./AddColumnInput"
+
 
 class Swimlane extends React.Component{
 
@@ -13,19 +15,31 @@ class Swimlane extends React.Component{
     state = {
         columnid: 0,
         columns: [
-            {id: "0", wiplimit: null}
+            {id: "0", wiplimit: null, title: "Backlog"}
 
         ]
 
         
     }
+
     
-    addColumn(){
+    
+    droppableStyle = {
+        backgroundColor: "rgb(70,83,98)",
+        height: "250px",
+        width: "200px",
+        margin: "15px",
+        float: "left",
+        borderRadius: "20px"
+    }
+    
+    addColumn(v){
         this.setState({columnid: this.state.columnid +1 })
         
         const item = {
             id: this.state.columnid,
-            wiplimit: null
+            wiplimit: null,
+            title: v
         }
 
         const newElements = [...this.state.columns, item]
@@ -33,20 +47,24 @@ class Swimlane extends React.Component{
 
     }
 
+    
+
 
     render(){
         const elements = this.state.columns.map(e => {
-            return <Droppable id={e.id} style={{backgroundColor: 'red', width: "100px", height: "100%", float: "left", margin: "5px"}}></Droppable>
+            return <Droppable id={e.id} style={this.droppableStyle}><input style={{width: "70%", display: "block", marginLeft: "auto", marginRight: "auto"}}value={e.title}></input></Droppable>
         })
+
+
 
        return (
            <div>
             <Collapsible trigger={<div><Popup trigger={<button>Trigger</button>} position="right center">
             <input style={{width: "150px", height: "20px"}}type="text" defaultValue={this.props.element.title}></input>
             <button onClick={() => this.props.removeSwimlane(this.props.element)}>Remove swimlane</button>
-            <button onClick={() => this.addColumn(this.props.element)}>Add column</button></Popup></div>}>
-                <Draggable id="item1"><div style={{backgroundColor: 'gray', width: '100px'}}>Some text</div></Draggable>
-                <div style={{height: "300px", backgroundColor: "#f3e5ab"}}>{elements}</div>
+            </Popup></div>}>
+                
+                    <div style={{height: "300px", /*backgroundColor: "#ccc"*/}}>{elements}<div style={{backgroundColor: "rgb(70,83,98)", width: "200px", height: "50px", float: "left", margin: "15px", borderRadius: "20px"}}><AddColumnInput addColumn={this.addColumn.bind(this)}/></div></div>
             </Collapsible>
             </div>
        )
