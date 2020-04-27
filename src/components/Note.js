4abae91812410d772b6696d97450b022fd57e67c
pjn98 <pasza03@wp.pl>
2020-04-27 17:19:18 +0200

    add progress bar to note

--- src/components/Note.js
+++ src/components/Note.js
@@ -6,6 +6,7 @@ import Popup from 'reactjs-popup'
 import avatar1 from '../avatars/avatar1.png'
 import avatar2 from '../avatars/avatar2.png'
 import avatar3 from '../avatars/avatar3.png'
+import Progress from "./Progress";
 import defaultAvatar from '../avatars/default.png'
 const Note = (props) => {
     //let [avatar,setAvatar] = useState(props.item);  moze sie przyda na pozniej
@@ -29,8 +30,10 @@ const Note = (props) => {
                             <button onClick={()=>handleClick(avatar1)} style={{visibility:"hidden"}}> <img src={avatar1} style={{borderRadius:50+"%", width:40,height:40,visibility:"visible"}}/>   </button>
                             <button onClick={()=>handleClick(avatar2)} style={{visibility:"hidden"}}> <img src={avatar2} style={{borderRadius:50+"%", width:40,height:40,visibility:"visible"}}/>   </button>
                             <button onClick={()=>handleClick(avatar3)} style={{visibility:"hidden"}}> <img src={avatar3} style={{borderRadius:50+"%", width:40,height:40,visibility:"visible"}}/>   </button>
+                            <Progress/>
                         </div>
                     </Popup>
+                    <Progress/>
                 </div>
             )}
         </Draggable>
