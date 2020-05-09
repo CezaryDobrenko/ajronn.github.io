935638fb1b311d444ed31865f0929351748c5a79
ajronn <34678770+ajronn@users.noreply.github.com>
2020-05-10 01:09:37 +0200

    +

--- src/components/Swimlane.js
+++ src/components/Swimlane.js
@@ -22,7 +22,7 @@ class Swimlane extends React.Component{
        return (
            <div style={{backroundColor:"#7BAEFF"}}>
             <Collapsible open triggerStyle={{backgroundColor: "rgb(70,83,98)", height: "50px"}}>
-                <div style={{width: "100%", height: "100%"}}>
+                <div>
                 
                     <div className="steericon" onClick={() => this.props.removeSwimlane(this.props.element)}><img src={SwimlaneIcon} height="30"/></div>
                     <input type="text" style={{all:"initial",width: "200px",marginLeft:"20px",backgroundColor:"transparent",textAlign:"center",border:"2px solid #009900",borderRadius:"10px",height:"30px",fontFamily: "'Comic Neue', cursive",marginBottom:"20px"}} defaultValue={this.props.element.title} onChange={(event) => this.props.changeSwimlaneTitle(this.props.element, event.target.value)} /></div>
